f012914acad75cc3dd9ab82292d4b48f4718967a
shambhavishashi <shambhavishashi@gmail.com>
2017-03-18 23:39:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('article1',function(req,res){
+res.send('this is article1')});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
